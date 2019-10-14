23ecaac1048a6fba4082a7e5a91f1ef4789ea005
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-10-14 19:04:03 +0000

    number and greater

---
+++ number_and_greater.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Created by: Teddy Sannan
+# Created on: October 2019
+# This takes two numbers
+#  and prints which is greater
+
+
+def main():
+    while True:
+        try:
+            number1 = int(input("Please enter your first number: "))
+            number2 = int(input("Please enter your second number: "))
+
+            if number1 > number2:
+                print()
+                print(number1, "is greater")
+
+            elif number2 > number1:
+                print()
+                print(number2, "is greater")
+
+            else:
+                print()
+                print("Both numbers are equal")
+
+        except ValueError:
+            print()
+            print("Invalid Input")
+            print()
+            continue
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
